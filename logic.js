a708c1310f7513e92cfafb0b3e57e59ca3707acd
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-07-30 18:12:48 -0300

    botões ficam verdinhos de volta

--- logic.js
+++ logic.js
@@ -1,31 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const allButtons = document.getElementsByTagName('button')
 
-    // ...
+    // Clicou no correto
     let correct = document.querySelector('.correct');
     correct.addEventListener('click', function() {
-        correct.style.backgroundColor = '#2E8B57';
-        document.querySelector('#feedback1').innerHTML = 'Acertou!';
+        for (button of Array.from(allButtons)) {
+            button.style.backgroundColor = '#80b060'
+        }
+        correct.style.backgroundColor = '#2E8B57'
+        document.querySelector('#feedback1').innerHTML = 'Acertou!'
     });
 
-    // ...
+    // Clicou em algum dos incorretos
     let incorrects = document.querySelectorAll('.incorrect');
     for (let x = 0; x < incorrects.length; x++) {
         incorrects[x].addEventListener('click', function() {
-            incorrects[x].style.backgroundColor = '#ff4c4c';
-            document.querySelector('#feedback1').innerHTML = 'Errou!';
+            for (button of Array.from(allButtons)) {
+                button.style.backgroundColor = '#80b060'
+            }
+            incorrects[x].style.backgroundColor = '#ff4c4c'
+            document.querySelector('#feedback1').innerHTML = 'Errou!'
         });
     }
 
-    // ...
+    // Responde o palpite da questão aberta
     document.querySelector('#check').addEventListener('click', function() {
-        let input = document.querySelector('input');
-        if (input.value === 'Irlanda' || input.value === 'irlanda' || input.value === 'IRLANDA') {
-            input.style.backgroundColor = '#2E8B57';
-            document.querySelector('#feedback2').innerHTML = 'Acertou!';
+        let input = document.querySelector('input')
+        if (input.value.toLowerCase() === 'irlanda' || 'republica da irlanda' || 'república da irlanda') {
+            input.style.backgroundColor = '#2E8B57'
+            document.querySelector('#feedback2').innerHTML = 'Acertou!'
         } else {
-            input.style.backgroundColor = '#ff4c4c';
-            document.querySelector('#feedback2').innerHTML = 'Errou!';
+            input.style.backgroundColor = '#ff4c4c'
+            document.querySelector('#feedback2').innerHTML = 'Errou!'
         }
-    });
+    })
 
-});
+})
